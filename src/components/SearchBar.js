675b0280ba8f66276140300c42486adacf4b28d5
Dmitry Sky <dmitry.sky@icloud.com>
2016-11-04 11:51:16 -0600

    simplify SearchBar component since it no longer handles state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { term: '' }
-  }
 
   onInputChange(term) {
-    this.setState({term});
     this.props.onTermChange(term);
   }
 
   render () {
     return (
-      <div>
+      <div className="search">
         <input placeholder="Enter Text To Search For Giffs!"
                onChange={event => this.onInputChange(event.target.value)} />
       </div>
